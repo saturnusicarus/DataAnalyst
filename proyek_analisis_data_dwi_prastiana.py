7c96849f04f6e336a257fafdbad3adeee048827f
Dwi Prastiana <59010467+saturnusicarus@users.noreply.github.com>
2023-12-17 22:43:30 +0700

    Update proyek_analisis_data_dwi_prastiana.py

--- proyek_analisis_data_dwi_prastiana.py
+++ proyek_analisis_data_dwi_prastiana.py
@@ -22,7 +22,7 @@ Original file is located at
 
 import pandas as pd
 import numpy as np
-import matplotlib as mpl
+##import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 
